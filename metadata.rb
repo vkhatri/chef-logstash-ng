397f7b01fd6c1262b798ac8c83e871f98cc433f8
Virender Khatri <vir.khatri@gmail.com>
2015-08-04 17:30:00 +0530

    bump cookbook version to v0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures logstash-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 %w(java yum apt).each do |d|
   depends d
